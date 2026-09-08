a0046ccc979448cf4ad0fbf781519f0547ac42c0
Fares Ayman <fareshe73@gmail.com>
2026-09-08 05:08:29 +0300

    fix issue 1

--- server.js
+++ server.js
@@ -7,9 +7,17 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./index');
 const DB = process.env.DATABASE_URL;
-mongoose.connect(DB).then(() => {
-  console.log('Connect Database Successfuly🚀');
-});
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('Connect Database Successfully 🚀');
+  })
+  .catch((err) => {
+    console.log('DATABASE CONNECTION ERROR 💥');
+    console.log(err.name);
+    console.log(err.message);
+  });
 
 const PORT = process.env.PORT;
 const server = app.listen(PORT, () => {
