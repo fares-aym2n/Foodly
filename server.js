a2f92136724bffc3dfc5c2bbd0e2ecf8a95e2e14
Fares Ayman <fareshe73@gmail.com>
2026-09-08 05:25:50 +0300

    add vercel file to deploy project

--- server.js
+++ server.js
@@ -7,17 +7,9 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./index');
 const DB = process.env.DATABASE_URL;
-
-mongoose
-  .connect(DB)
-  .then(() => {
-    console.log('Connect Database Successfully 🚀');
-  })
-  .catch((err) => {
-    console.log('DATABASE CONNECTION ERROR 💥');
-    console.log(err.name);
-    console.log(err.message);
-  });
+mongoose.connect(DB).then(() => {
+  console.log('Connect Database Successfuly🚀');
+});
 
 const PORT = process.env.PORT;
 const server = app.listen(PORT, () => {
